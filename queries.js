e8f0f552716a7d7e9ce98e027464468d98a84807
rubansi <rubansi24@gmail.com>
2025-11-09 18:05:45 +0300

    task submission

--- queries.js
+++ queries.js
@@ -16,3 +16,24 @@ db.books.updateOne({ title: "1984" }, { $set: { price: 17.99 } })
 db.books.deleteOne({ title: "Becoming" })
 
 
+// AdVanced Queries
+// Books in stock and published after 2010
+db.books.find({ in_stock: true, published_year: { $gt: 2010 } })
+
+// Projection - return only title, author, and price
+db.books.find({}, { title: 1, author: 1, price: 1, _id: 0 })
+
+// Sorting by price ascending
+db.books.find().sort({ price: 1 })
+
+// Sorting by price descending
+db.books.find().sort({ price: -1 })
+
+// Pagination: 5 books per page
+// Page 1
+db.books.find().limit(5)
+// Page 2
+db.books.find().skip(5).limit(5)
+
+
+
