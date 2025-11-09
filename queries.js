2b9a2844b18294b35d6470386afec084cac3859c
rubansi <rubansi24@gmail.com>
2025-11-09 18:06:39 +0300

    task submission

--- queries.js
+++ queries.js
@@ -60,4 +60,13 @@ db.books.aggregate([
   { $sort: { _id: 1 } }
 ])
 
+// indexing
+// Create index on title
+db.books.createIndex({ title: 1 })
+
+// Create compound index on author and published_year
+db.books.createIndex({ author: 1, published_year: -1 })
+
+// Use explain() to analyze query performance
+db.books.find({ title: "Sapiens" }).explain("executionStats")
 
